412c05f5742c4dc30a399761203d13e6718ce42f
윤주찬 <wncks2546@gmail.com>
2024-01-07 23:36:11 +0900

    nomad movie start;

--- a.py
+++
@@ -1,9 +0,0 @@
-T =int(input())
-
-for test_case in range(1, T+1):
-    numbers = list(map( int, input().split() ) )
-    sum = 0
-    for i in range(len(numbers)):
-        if numbers[i]%2==1:
-            sum+=numbers[i]
-    print(f'#{test_case} {sum}')
