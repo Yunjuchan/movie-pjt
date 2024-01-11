8fed9151474dd0529c8e83b943bde083e7b5d523
윤주찬 <wncks2546@gmail.com>
2024-01-12 00:28:26 +0900

    리액트 리듀서 실습

--- practice-pjt/src/components/Counter.js
+++ practice-pjt/src/components/Counter.js
@@ -1,20 +1,41 @@
-import { createStore } from "redux";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../store/counterSlice";
 
 function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-    function counter(state = 0, action) {
-        switch (action.type) {
-          case 'INCREMENT':
-            return state + 1
-          case 'DECREMENT':
-            return state - 1
-          default:
-            return state
-        }
-      }
-      
-    return;
+  function counter(state = 0, action) {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      default:
+        return state;
+    }
+  }
 
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{ count }</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Counter;
